0d7e294b83df2beff34f5038bc0a71d8d67447c6
karsten9 <57909814+karsten9@users.noreply.github.com>
2020-01-27 15:57:16 +0100

    Update devicemotion.js

--- devicemotion.js
+++ devicemotion.js
@@ -1,13 +1,13 @@
 "use strict";
 
 // Inhalt des Scripts devicemotion.js:
+let maxi = -1;
 if (window.DeviceMotionEvent) {
     document.getElementById("dmeSupported").innerText = "OK - Device Motion wird unterstützt!";
     window.addEventListener('devicemotion', function(event) {
     document.getElementById("xBeschl").innerHTML = 'x ' + event.acceleration.x.toFixed(2);
     document.getElementById("yBeschl").innerHTML = 'y ' + event.acceleration.y.toFixed(2);
     document.getElementById("zBeschl").innerHTML = 'z ' + event.acceleration.z.toFixed(2);
-    let maxi = -1;
     if (event.acceleration.z > maxi) {
     maxi = event.acceleration.z;
     document.getElementById("meinMax").innerHTML = 'max z: ' + maxi;
