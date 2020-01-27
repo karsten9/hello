a0fe8cc9580ffd5c4560625e043d384224e2e26f
karsten9 <57909814+karsten9@users.noreply.github.com>
2020-01-27 15:55:29 +0100

    Update devicemotion.js

--- devicemotion.js
+++ devicemotion.js
@@ -7,13 +7,12 @@ if (window.DeviceMotionEvent) {
     document.getElementById("xBeschl").innerHTML = 'x ' + event.acceleration.x.toFixed(2);
     document.getElementById("yBeschl").innerHTML = 'y ' + event.acceleration.y.toFixed(2);
     document.getElementById("zBeschl").innerHTML = 'z ' + event.acceleration.z.toFixed(2);
+    if (event.acceleration.z > maxi) {
+    maxi = event.acceleration.z;
+    document.getElementById("meinMax").innerHTML = 'max z: ' + maxi;
+    }
     });
     } else {
     document.getElementById("dmeSupported").innerText = "Device Motion wird nicht unterstützt!";
     }
 let maxi = -1;
-if (event.acceleration.z > maxi) {
-    alert (event.acceleration.z);
-maxi = event.acceleration.z;
-document.getElementById("meinMax").innerHTML = 'max z: ' + maxi;
-}
